0d1ecdda193bafbfc06450892933f8524f11ffd8
Julio Eduardo Gustavo Alvarado Ramirez <128263541+julioalvarado202307764@users.noreply.github.com>
2023-11-15 12:09:41 -0600

    Example 2 arrays and foreach

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿/*int [] inventory = {200, 450, 700, 175, 250 }; FOREACH UTILIZANDO INT e incremento en uno en la variable bin
+
+int sum = 0;
+int bin = 0;
+foreach(int items in inventory){
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin{bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory");*/
+string [] pedidos = {"B123","C234", "A345", "C15", "B177", "G3003", "C235", "B179"};//LLamar a todos los pedidos que comienzen con la letra B
+foreach(string pedido in pedidos){
+    if(pedido.StartsWith("B")){
+        Console.WriteLine($"Los pedidos que inician con la letra B son: {pedido}");
+    }
+}
